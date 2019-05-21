4adf5447acd54765baf1ab187ca3f2e7cbbb366f
fieldr <fieldr92@gmail.com>
2019-05-21 12:20:45 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import UserCreate from './UserCreate';
+
+export default class App extends Component {
+  state = {
+    language: 'english'
+  };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag uk"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
