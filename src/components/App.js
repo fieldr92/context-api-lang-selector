515894455b4b185457eccdfad3c947180dbb8df9
fieldr <fieldr92@gmail.com>
2019-05-21 13:04:14 +0100

    Example to show Provider can be reused

--- src/components/App.js
+++ src/components/App.js
@@ -28,6 +28,10 @@ export default class App extends Component {
         <LanguageContext.Provider value={this.state.language}>
           <UserCreate />
         </LanguageContext.Provider>
+        <LanguageContext.Provider value="dutch">
+          <UserCreate />
+        </LanguageContext.Provider>
+        <UserCreate />
       </div>
     );
   }
