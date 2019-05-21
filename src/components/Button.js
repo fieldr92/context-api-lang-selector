999bf434293f871d3b6985d50dff781613da0b15
fieldr <fieldr92@gmail.com>
2019-05-21 12:30:00 +0100

    Add default context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import LangaugeContext from '../contexts/LanguageContext';
 
 export default class Button extends Component {
+  static contextType = LangaugeContext; // has to be called 'contextType'
+
   render() {
+    console.log(this.context);
     return <button className="ui button primary">Submit</button>;
   }
 }
