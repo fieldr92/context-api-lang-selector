f30c05f282038c859f958ed416e8279336a70c79
fieldr <fieldr92@gmail.com>
2019-05-21 12:45:07 +0100

    Add Provider component

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ export default class Button extends Component {
   static contextType = LangaugeContext; // has to be called 'contextType'
 
   render() {
-    console.log(this.context);
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return <button className="ui button primary">{text}</button>;
   }
 }
